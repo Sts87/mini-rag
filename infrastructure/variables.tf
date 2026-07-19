be6101759271087ff56fa0391c603d42c2cbe619
Sts87 <134673338+Sts87@users.noreply.github.com>
2026-07-19 19:47:30 +0000

    chore: add terraform variables for compute and network

---
+++ infrastructure/variables.tf
@@ -0,0 +1,42 @@
+variable "oci_profile" {
+    type = string
+}
+
+variable "tenancy_ocid" {
+    type = string
+}
+
+variable "vcn_display_name" {
+    type = string
+}
+
+variable "vcn_dns_label" {
+    type = string
+}
+
+variable "vcn_cidr_blocks" {
+    type = list(string)
+}
+
+variable "public_subnet_dns_label" {
+    type = string
+}
+
+variable "instance_shape" {
+    type = string
+    default = "VM.Standard.E2.1.Micro"
+}
+
+variable "instance_metadata" {
+    type = map(string)
+}
+
+variable "instance_ocpus" {
+    type = number
+    default = 1
+}
+
+variable "instance_memory_in_gbs" {
+    type = number
+    default = 1
+}
